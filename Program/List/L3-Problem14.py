b05330118732907fb9d7f78d8a8006d23514887b
Sumaiya Bushra <100853902+suubx@users.noreply.github.com>
2026-07-11 20:33:31 +0600

    Add files via upload

---
+++ Program/List/L3-Problem14.py
@@ -0,0 +1,6 @@
+numbers = [100, 20, 350, 410, 530, 670, 790, 840]
+
+print("First three elements:", numbers[:3])
+print("Last three elements:", numbers[-3:])
+print("Every second element:", numbers[::2])
+print("Reverse order:", numbers[::-1])
