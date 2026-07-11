6570a54c3d8f2fc2f34c3cc3b824596d3f866745
Sumaiya Bushra <100853902+suubx@users.noreply.github.com>
2026-07-11 20:30:05 +0600

    Add files via upload

---
+++ Program/List/L3-Problem12.py
@@ -0,0 +1,13 @@
+students = ["Aona", "Piya", "Orin", "Eysha"]
+
+students.append("Ana")
+print("After adding:", students)
+
+students.insert(1, "Hasan")
+print("After inserting:", students)
+
+students.remove("Piya")
+print("After removing:", students)
+
+students.sort()
+print("Alphabetical order:", students)
